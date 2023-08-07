9ab413ccc34c8df870750a7d4530757baba01a44
liuzhipeng <liuzhipengdongjing@gmail.com>
2023-08-07 22:12:37 +0900

    change mapper ➡️ mapperscan

--- src/main/java/com/ffl/myapp01boot/MyApp01BootApplication.java
+++ src/main/java/com/ffl/myapp01boot/MyApp01BootApplication.java
@@ -1,9 +1,11 @@
 package com.ffl.myapp01boot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ffl.myapp01boot.mapper")
 public class MyApp01BootApplication {
 
     public static void main(String[] args) {
